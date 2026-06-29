d933960548587ab8661812f6b821e8d63efa937b
mrizalz77-source <mrizalz77@gmail.com>
2026-06-29 23:23:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -95,5 +95,8 @@ def logout():
     return redirect(url_for("login"))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
